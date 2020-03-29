1e5e6cac39c60d7a8ae9d50c2b90beba7b85a613
Gustavo Ariadno <gugu.gustavo2011@gmail.com>
2020-03-29 01:13:36 -0300

    btoi now works with spaces

--- ft_btoi.c
+++ ft_btoi.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdlib.h>
 
-int	ft_strlen(char *string)
+size_t	ft_strlen(char *string)
 {
 	int i = 0;
 	while (string[i])
@@ -8,31 +8,31 @@ int	ft_strlen(char *string)
 	return (i);
 }
 
-int ft_pow(int base, int power)
+size_t	ft_pow(int base, int pow)
 {
 	int x = base;
 
-	if (power == 0)
+	if (pow == 0)
 		return (1);
-	else if (power == 1)
+	else if (pow == 1)
 		return (base);
-	while (power-- > 1)
+	while (pow-- > 1)
 		x *= base;
 	return (x);
 }
 
-int ft_btoi(char *str)
+size_t	ft_btoi(char *str)
 {
-	int result = 0;
-	int pow = 0;
-	int i = 0;
+	size_t	result = 0;
+	size_t	pow = 0;
+	size_t	i = 0;
 
 	pow = ft_strlen(str) - 1;
 	while (str[i])
 	{
 		if (str[i++] == '1')
 			result += ft_pow(2, pow);
-		pow--; 
+		(str[i] == '0') ? pow-- : 0; 
 	}
 	return (result);
 }
