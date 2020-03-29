8050a784c3f43d1c18d06876f907a924352bcc5d
Gustavo Ariadno <gugu.gustavo2011@gmail.com>
2020-03-29 01:25:20 -0300

    bugs fixed

--- ft_btoi.c
+++ ft_btoi.c
@@ -1,11 +1,19 @@
 #include <stdlib.h>
 
-size_t	ft_strlen(char *string)
+size_t	check_valid(char *str)
 {
-	int i = 0;
-	while (string[i])
+	int	valid;
+	int	i;
+	
+	valid = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '0' || str[i] == '1')
+			valid++;
 		i++;
-	return (i);
+	}
+	return (valid);
 }
 
 size_t	ft_pow(int base, int pow)
@@ -27,12 +35,14 @@ size_t	ft_btoi(char *str)
 	size_t	pow = 0;
 	size_t	i = 0;
 
-	pow = ft_strlen(str) - 1;
+	pow = check_valid(str) - 1;
 	while (str[i])
 	{
-		if (str[i++] == '1')
+		if (str[i] == '1')
 			result += ft_pow(2, pow);
-		(str[i] == '0') ? pow-- : 0; 
+		if (str[i] == '1' || str[i] == '0')
+			pow--; 
+		i++;
 	}
 	return (result);
 }
--- main.c
+++ main.c
@@ -4,5 +4,5 @@ int ft_btoi(char *str);
 
 int main(void)
 {
-	printf("%d\n", ft_btoi("01010101"));
+	printf("%d\n", ft_btoi("11111111"));
 }
